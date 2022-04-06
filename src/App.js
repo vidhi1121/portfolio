abf6f66296883bc62ec96ef0e20745c49a87fd33
Vidhi Davda <77352943+vidhi1121@users.noreply.github.com>
2022-04-06 13:20:42 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import Preloader from "./src/components/Pre";
-import Navbar from "./src/components/Navbar";
-import Home from "./src/components/Home/Home";
-import About from "./src/components/About/About";
-import Projects from "./src/components/Projects/Projects";
-import Footer from "./src/components/Footer";
+import Preloader from "./components/Pre";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer";
 
 import {
   BrowserRouter as Router,
@@ -12,11 +12,11 @@ import {
   Routes,
   Navigate
 } from "react-router-dom";
-import ScrollToTop from "./src/components/ScrollToTop";
+import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Certificates from "./src/components/Certificates/Certificates";
+import Certificates from "./components/Certificates/Certificates";
 
 function App() {
   const [load, upadateLoad] = useState(true);
